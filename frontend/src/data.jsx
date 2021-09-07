52899f49f3290df0af7c592f8f395ac347136b5e
Skill Actz <info@trinityskillworks.com>
2021-09-07 22:48:47 +0530

    react set up completed and products are listed using map function

---
+++ frontend/src/data.jsx
@@ -0,0 +1,78 @@
+const data ={
+    products:[
+        {
+            _id :'1',
+            name : 'Nike Hand Bag',
+            category : 'shirts',
+            image : 'images/bag.jpg',
+            price : 120,
+            brand : 'Nike',
+            rating : 4.5,
+            numReviews : 10,
+            description : 'high quality product',
+            
+        },
+        {
+            _id :'2',
+            name : 'Nike Hand Bag2',
+            category : 'shirts',
+            image : 'images/bag.jpg',
+            price : 100,
+            brand : 'Nike',
+            rating : 4.5,
+            numReviews : 10,
+            description : 'high quality product',
+            
+        },
+        {
+            _id :'3',
+            name : 'Nike Hand Bag3',
+            category : 'shirts',
+            image : 'images/bag.jpg',
+            price : 125,
+            brand : 'Nike',
+            rating : 3.5,
+            numReviews : 10,
+            description : 'high quality product',
+            
+        },
+        {
+            _id :'4',
+            name : 'Nike Hand Bag4',
+            category : 'shirts',
+            image : 'images/bag.jpg',
+            price : 150,
+            brand : 'Nike',
+            rating : 4,
+            numReviews : 10,
+            description : 'high quality product',
+            
+        },
+        {
+            _id :'5',
+            name : 'Nike Hand Bag5',
+            category : 'shirts',
+            image : 'images/bag.jpg',
+            price : 180,
+            brand : 'Nike',
+            rating : 3.9,
+            numReviews : 10,
+            description : 'high quality product',
+            
+        },
+        {
+            _id :'6',
+            name : 'Nike Hand Bag6',
+            category : 'shirts',
+            image : 'images/bag.jpg',
+            price : 128,
+            brand : 'Nike',
+            rating : 4.7,
+            numReviews : 10,
+            description : 'high quality product',
+            
+        }
+]
+}
+
+export default data;
