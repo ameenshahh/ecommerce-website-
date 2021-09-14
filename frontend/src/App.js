23926c0b1f3c6b06e1ea27f84d3fa8daef9073b5
Skill Actz <info@trinityskillworks.com>
2021-09-14 23:15:50 +0530

    built product page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import data from './data'
-import Product from './components/Product'
+import { BrowserRouter, Route } from 'react-router-dom';
+import ProductScreen from './screens/ProductScreen';
+import HomeScreen from './screens/HomeScreen';
 
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <div className="grid-container">
         <header className="row">
           <div>
@@ -17,20 +18,16 @@ function App() {
           </div>
         </header>
         <main>
-          <div className="row center">
-            {
-              data.products.map(product =>(
-                <Product key={product._id} product={product}></Product>
-              ))
-            }
-                
-            
-          </div>
+          <Route path='/product/:id' component={ProductScreen} ></Route>
+          <Route path='/' component={HomeScreen} exact></Route>
+          
         </main>
         <footer className="row center">All Rights Reserved</footer>
       </div>
-    </div>
+      </BrowserRouter>
+    
   );
 }
 
 export default App;
+
