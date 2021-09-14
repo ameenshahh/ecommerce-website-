03483b470f32e4f8c169fd6b552a07e2ddc48a01
Skill Actz <info@trinityskillworks.com>
2021-09-15 00:02:19 +0530

    basic node and express server setup

---
+++ backend/data.js
@@ -0,0 +1,84 @@
+const data ={
+    products:[
+        {
+            _id :'1',
+            name : 'Nike Hand Bag',
+            category : 'shirts',
+            image : 'images/bag.jpg',
+            price : 120,
+            countInStock : 10,
+            brand : 'Nike',
+            rating : 4.5,
+            numReviews : 10,
+            description : 'high quality product',
+            
+        },
+        {
+            _id :'2',
+            name : 'Nike Hand Bag2',
+            category : 'shirts',
+            image : 'images/bag.jpg',
+            price : 100,
+            countInStock : 20,
+            brand : 'Nike',
+            rating : 4.5,
+            numReviews : 16,
+            description : 'high quality product',
+            
+        },
+        {
+            _id :'3',
+            name : 'Nike Hand Bag3',
+            category : 'shirts',
+            image : 'images/bag.jpg',
+            price : 125,
+            countInStock : 0,
+            brand : 'Nike',
+            rating : 3.5,
+            numReviews : 24,
+            description : 'high quality product',
+            
+        },
+        {
+            _id :'4',
+            name : 'Nike Hand Bag4',
+            category : 'shirts',
+            image : 'images/bag.jpg',
+            price : 150,
+            countInStock : 30,
+            brand : 'Nike',
+            rating : 4,
+            numReviews : 3,
+            description : 'high quality product',
+            
+        },
+        {
+            _id :'5',
+            name : 'Nike Hand Bag5',
+            category : 'shirts',
+            image : 'images/bag.jpg',
+            price : 180,
+            countInStock : 40,
+            brand : 'Nike',
+            rating : 3.9,
+            numReviews : 33,
+            description : 'high quality product',
+            
+        },
+        {
+            _id :'6',
+            name : 'Nike Hand Bag6',
+            category : 'shirts',
+            image : 'images/bag.jpg',
+            price : 128,
+            countInStock : 50,
+            brand : 'Nike',
+            rating : 4.7,
+            numReviews : 29,
+            description : 'high quality product',
+            
+        }
+]
+}
+
+export default data;
